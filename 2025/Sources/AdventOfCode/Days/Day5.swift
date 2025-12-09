bda6e6a9cb0cc7b2c9132d9673c71228fcc0e25d
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2025-12-09 17:25:37 +0100

    Day 5 end

--- 2025/Sources/AdventOfCode/Days/Day5.swift
+++ 2025/Sources/AdventOfCode/Days/Day5.swift
@@ -4,9 +4,96 @@ import Foundation
 struct Day5 {
     let identifier = "Day5"
 
-    func run(from filePath: URL) throws -> Int {
-        let content = try String(contentsOf: filePath, encoding: .utf8)
+    struct Database: Equatable {
+        let freshIDRanges: [ClosedRange<Int>]
+        let availableIDs: [Int]
+    }
 
-        return 0
+    func run(from filePath: URL) throws -> Int {
+        let inputText = try String(contentsOf: filePath, encoding: .utf8)
+        let database = try parsedDatabase(from: String(inputText.dropLast()))
+
+        return numberOfPossibleIDs(in: database)
+    }
+
+    func parsedDatabase(from input: String) throws -> Database {
+        try ParsableDataBase().parse(input)
+    }
+
+    func numberOfFreshAvailableIDs(in database: Database) -> Int {
+        database
+            .getAvailableFreshIDs()
+            .count
+    }
+
+    func numberOfPossibleIDs(in database: Database) -> Int {
+        database
+            .getMergedFreshIDRanges()
+            .reduce(0) { $0 + $1.count }
+    }
+}
+
+extension Day5.Database {
+    func getAvailableFreshIDs() -> [Int] {
+        availableIDs
+            .filter { id in
+                freshIDRanges.contains(where: { $0.contains(id) })
+            }
+    }
+
+    func getMergedFreshIDRanges() -> [ClosedRange<Int>] {
+        freshIDRanges
+            .sorted(by: { $0.lowerBound < $1.lowerBound })
+            .reduce([ClosedRange<Int>]()) { result, range in
+                guard let lastRange = result.last else {
+                    return [range]
+                }
+
+                if lastRange.overlaps(range) {
+                    let newRange = lastRange.union(with: range)
+
+                    return result.dropLast() + [newRange]
+                } else {
+                    return result + [range]
+                }
+            }
+    }
+}
+
+extension ClosedRange<Int> {
+    func union(with other: Self) -> Self {
+        Swift.min(lowerBound, other.lowerBound)...Swift.max(upperBound, other.upperBound)
+    }
+}
+
+struct ParsableDataBase: Parser {
+    var ranges: some Parser<Substring, [ClosedRange<Int>]> {
+        Many {
+            Parse {
+                $0...$1
+            } with: {
+                Int.parser()
+                "-"
+                Int.parser()
+            }
+        } separator: {
+            "\n"
+        }
+    }
+
+    var availableIDs: some Parser<Substring, [Int]> {
+        Many {
+            Int.parser()
+        } separator: {
+            "\n"
+        }
+    }
+
+    var body: some Parser<Substring, Day5.Database> {
+        Parse(Day5.Database.init(freshIDRanges:availableIDs:)) {
+            ranges
+            "\n\n"
+            availableIDs
+        }
     }
 }
--- 2025/Tests/AdventOfCodeTests/Day5Tests.swift
+++ 2025/Tests/AdventOfCodeTests/Day5Tests.swift
@@ -2,5 +2,57 @@
 import Testing
 
 struct Day5Tests {
-    
+    @Test func databaseParsing() throws {
+        let input = """
+            3-5
+            10-14
+            16-20
+            12-18
+
+            1
+            5
+            8
+            11
+            17
+            32
+            """
+
+        let sut = Day5()
+        let expected = Day5.Database.testDatabase
+
+        try #expect(sut.parsedDatabase(from: input) == expected)
+    }
+
+    @Test func databaseAvailableFreshIDs() {
+        let sut = Day5.Database.testDatabase
+        let expected = [5, 11, 17]
+
+        #expect(sut.getAvailableFreshIDs() == expected)
+    }
+
+    @Test func databaseAllPossibleFreshIDs() {
+        let sut = Day5.Database.testDatabase
+        let expected = [
+            3...5,
+            10...20
+        ]
+        let result = sut.getMergedFreshIDRanges()
+            .sorted(by: { $0.lowerBound < $1.lowerBound })
+
+        #expect(result == expected)
+    }
+}
+
+extension Day5.Database {
+    static var testDatabase: Self {
+        Day5.Database(
+            freshIDRanges: [
+                3...5,
+                10...14,
+                16...20,
+                12...18
+            ],
+            availableIDs: [1, 5, 8, 11, 17, 32]
+        )
+    }
 }
