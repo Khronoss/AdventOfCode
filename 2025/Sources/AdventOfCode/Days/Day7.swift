dd67c96b678414a4b9563a189ea10dc306fbd942
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2025-12-10 15:50:00 +0100

    Day 7 - setup

---
+++ 2025/Sources/AdventOfCode/Days/Day7.swift
@@ -0,0 +1,12 @@
+import Parsing
+import Foundation
+
+struct Day7 {
+    let identifier = "Day7"
+
+    func run(from filePath: URL) throws -> Int {
+        let inputText = try String(contentsOf: filePath, encoding: .utf8)
+
+        return 0
+    }
+}
