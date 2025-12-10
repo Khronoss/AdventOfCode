715a0673da4dde0f73aa67bc7274281e3f6b3681
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2025-12-10 17:41:59 +0100

    Day 7 - part 2

--- 2025/Sources/AdventOfCode/Days/Day7.swift
+++ 2025/Sources/AdventOfCode/Days/Day7.swift
@@ -5,28 +5,35 @@ struct Day7 {
     let identifier = "Day7"
 
     struct State: Equatable {
-        var beams: [Int]
-        var splitsCount: Int
+        let beams: [Int: Int]
+        var nextBeams: [Int: Int] = [:]
+        var splitsCount: Int = 0
 
         mutating func split(at beamIndex: Int) {
-            guard beams.contains(beamIndex) else { return }
+            guard let beamsCount = beams[beamIndex] else { return }
 
-            beams.removeAll(where: { $0 == beamIndex})
-
-            insertBeam(at: beamIndex - 1)
-            insertBeam(at: beamIndex + 1)
+            insertBeams(beamsCount, at: beamIndex - 1)
+            insertBeams(beamsCount, at: beamIndex + 1)
 
             splitsCount += 1
         }
 
         mutating func insertBeam(at index: Int) {
-            if !beams.contains(index) {
-                beams.append(index)
-            }
+            insertBeams(1, at: index)
         }
 
-        func hasBeam(at index: Int) -> Bool {
-            beams.contains(index)
+        mutating func insertBeams(_ count: Int, at index: Int) {
+            nextBeams[index] = (nextBeams[index] ?? 0) + count
+        }
+
+        mutating func moveBeam(at index: Int) {
+            nextBeams[index] = (beams[index] ?? 0) + (nextBeams[index] ?? 0)
+        }
+
+        var nextState: Self {
+            if nextBeams.isEmpty { return self }
+
+            return .init(beams: nextBeams, splitsCount: splitsCount)
         }
     }
 
@@ -38,10 +45,10 @@ struct Day7 {
 
     func runSimulation(for input: String) -> Int {
         let lines = input.split(separator: "\n")
-        let initialState = State(beams: [], splitsCount: 0)
+        let initialState = State(beams: [:])
         let result = process(rows: lines, state: initialState)
 
-        return result.splitsCount
+        return result.beams.reduce(0) { $0 + $1.value }
     }
 
     func process(rows: [some StringProtocol], state: State) -> State {
@@ -57,7 +64,11 @@ struct Day7 {
                     state.insertBeam(at: offset)
                 } else if char == "^" {
                     state.split(at: offset)
+                } else if char == "." {
+                    state.moveBeam(at: offset)
                 }
             }
+
+        state = state.nextState
     }
 }
--- 2025/Tests/AdventOfCodeTests/Day7Tests.swift
+++ 2025/Tests/AdventOfCodeTests/Day7Tests.swift
@@ -4,22 +4,22 @@ import Testing
 struct Day7Tests {
     @Test func createBeam() {
         let row = "..S..."
-        var state = Day7.State(beams: [], splitsCount: 0)
+        var state = Day7.State(beams: [:])
         let sut = Day7()
 
         sut.process(row: row, state: &state)
 
-        #expect(state == .init(beams: [2], splitsCount: 0))
+        #expect(state == .init(beams: [2: 1]))
     }
 
     @Test func processSplitBeam() {
         let row = ".^...^."
-        var state = Day7.State(beams: [5], splitsCount: 0)
+        var state = Day7.State(beams: [5: 1])
         let sut = Day7()
 
         sut.process(row: row, state: &state)
 
-        #expect(state == .init(beams: [4, 6], splitsCount: 1))
+        #expect(state == .init(beams: [4: 1, 6: 1], splitsCount: 1))
     }
 
     @Test func processMultipleRows() {
@@ -30,28 +30,28 @@ struct Day7Tests {
             "......",
             ".^.^.."
         ]
-        let state = Day7.State(beams: [], splitsCount: 0)
+        let state = Day7.State(beams: [:])
         let sut = Day7()
 
         let result = sut.process(rows: rows, state: state)
 
-        #expect(result == .init(beams: [0, 2, 4], splitsCount: 3))
+        #expect(result == .init(beams: [0: 1, 2: 2, 4: 1], splitsCount: 3))
     }
 
     @Test func stateSplit() {
-        var sut = Day7.State(beams: [2], splitsCount: 0)
+        var sut = Day7.State(beams: [2: 1])
 
         sut.split(at: 2)
 
-        #expect(sut == .init(beams: [1, 3], splitsCount: 1))
+        #expect(sut.nextState == .init(beams: [1: 1, 3: 1], splitsCount: 1))
     }
 
     @Test func stateSplitWithExistingBeam() {
-        var sut = Day7.State(beams: [2, 4], splitsCount: 0)
+        var sut = Day7.State(beams: [2: 1, 4: 1])
 
         sut.split(at: 2)
         sut.split(at: 4)
 
-        #expect(sut == .init(beams: [1, 3, 5], splitsCount: 2))
+        #expect(sut.nextState == .init(beams: [1: 1, 3: 2, 5: 1], splitsCount: 2))
     }
 }
